04f2de84ac204976220ebca5c43ed490b342f057
davidtkunz <117479775+davidtkunz@users.noreply.github.com>
2023-04-10 09:46:52 -0700

    Update jupytertest.spec.ts

--- .tests/tests/jupytertest.spec.ts
+++ .tests/tests/jupytertest.spec.ts
@@ -6,94 +6,102 @@ const demo_user_pw = 'wyiEwLP545sE5FY';
 
 const sleep = (ms) => new Promise(r => setTimeout(r, ms));
 
-test.only('verify multiple tabs', async({context})=>{
-    test.setTimeout(7200000);
-    const page = await context.newPage();
+test.describe('New Todo', () => {
+    test('verify multiple tabs', async({page})=>{
+        test.setTimeout(7200000);
+        //const page = await context.newPage();
 
-    // Login In  (Done!)
-    await page.goto('https://clearscape.teradata.com/dashboard');
-    await page.fill('input[id="email"]', emailaddr);
-    await page.fill('input[id="password"]', password);
-    await page.click('button >> text=LOG IN');
+        // Login In  (Done!)
+        await page.goto('https://clearscape.teradata.com/dashboard');
+        await page.fill('input[id="email"]', emailaddr);
+        await page.fill('input[id="password"]', password);
+        await page.click('button >> text=LOG IN');
 
-    // Check if Start Environment is disabled or not (Done)
-    //
-    //await page.waitForSelector('button:is([disabled]) >> text=RUN DEMOS USING JUPYTER');
-    if (await page.locator('button >> text=RUN DEMOS USING JUPYTER').isDisabled()){   //await page.locator('button >> text=START ENVIRONMENT').isEnabled()){
-        await page.locator('button >> text=START ENVIRONMENT').click();
-        await page.waitForSelector('button:not([disabled]) >> text=RUN DEMOS USING JUPYTER');
-    }
-    // Click to Run Demos (Done)
-    const [newPage] = await Promise.all([
-        context.waitForEvent('page'),
-        // This action triggers the new tab
-        page.locator('button >> text=RUN DEMOS USING JUPYTER').click() 
-      ])
-        // Wait for New Page/Tab to Load, Then jump over to new page (Done)
-        await newPage.waitForLoadState();                
-        // title of new tab page
-        //console.log(await newPage.title());       
-        // title of existing page
-        //console.log(await page.title());
-        //const jupyter_url=await newPage.url(); //get the url of the current page
-        //strip jupyter url 
-        //const jUrl = jupyter_url.split("/lab/")[0]
-
-        // Wait for content to appear before moving on.  Jupyter is slow to load (Done)
-        await newPage.waitForSelector('text=ClearScape Analytics Demonstrations via Jupyter');
-
-        // Run through Demos //
-
-        // GettingStarted
-        await runDemo(newPage,"GettingStarted","GettingStarted","GettingStarted.ipynb","false");
-        await runDemo(newPage,"GettingStarted","GettingStarted_Charting","GettingStarted_Charting.ipynb","false");   // ?              
-        await runDemo(newPage,"GettingStarted","GettingStarted_Jupyter_Pandas","GettingStarted_Jupyter_Pandas.ipynb","true");  // This python script has too many inputs
+        // Check if Start Environment is disabled or not (Done)
+        //
+        //await page.waitForSelector('button:is([disabled]) >> text=RUN DEMOS USING JUPYTER');
+        if (await page.locator('button >> text=RUN DEMOS USING JUPYTER').isDisabled()){   //await page.locator('button >> text=START ENVIRONMENT').isEnabled()){
+            await page.locator('button >> text=START ENVIRONMENT').click();
+            await page.waitForSelector('button:not([disabled]) >> text=RUN DEMOS USING JUPYTER');
+        }
         
-        // UseCases
-        await runDemo(newPage,"UseCases","AirPassengersTimeSeriesForecasting","AirPassengersTimeSeriesForecasting.ipynb","false");
-        await runDemo(newPage,"UseCases","AustinBikeShare","AustinBikeShare.ipynb","false");
-        await runDemo(newPage,"UseCases","BI_Tools","Dataiku.ipynb","true");
-        await runDemo(newPage,"UseCases","BrokenDigitalEvents","RetailBrokenDigitalJourneysPython.ipynb","true");
-        await runDemo(newPage,"UseCases","Clustering_KMeans","Clustering_KMeans.ipynb","false");
-        await runDemo(newPage,"UseCases","Consumption_Forecasting_BYOM","Energy_Consumption_Forecasting_BYOM.ipynb","true");
-        await runDemo(newPage,"UseCases","CreditCardFraud","CreditCardFraud.ipynb","false");
-        await runDemo(newPage,"UseCases","CustomerBehaviorAnalysis","BehavioralAnalysis.ipynb","true");
-        //await runDemo(newPage,"UseCases","Data_Dictionary","Data_Dictionary_Legacy.ipynb","true");   // hung up on space allocation
-        await runDemo(newPage,"UseCases","Data_Dictionary","Data_Dictionary.ipynb","true");  
-        await runDemo(newPage,"UseCases","DataPrepAndDiscovery","DataPrepAndDiscovery.ipynb","true"); 
-        await runDemo(newPage,"UseCases","DataScienceFlow","DataScienceFlow.ipynb","true"); 
-        await runDemo(newPage,"UseCases","DeepHistory","DeepHistory.ipynb","false");
-        await runDemo(newPage,"UseCases","ESG","ESG_pop.ipynb","true");
-        await runDemo(newPage,"UseCases","ESG","ESG.ipynb","true");
-        await runDemo(newPage,"UseCases","FourierTransform","FourierTransform.ipynb","false");
-        await runDemo(newPage,"UseCases","FSCustomerJourney","FSCustomerJourney.ipynb","false");
-        await runDemo(newPage,"UseCases","GLM_Fraud_Detection_BYOM","GLM_Fraud_Detection_BYOM.ipynb","true");
-        await runDemo(newPage,"UseCases","GLM_Fraud_Detection_InDB","GLM_Fraud_Detection_InDB.ipynb","true");
-        await runDemo(newPage,"UseCases","IndoorSensor","IndoorSensor.ipynb","true");
-        await runDemo(newPage,"UseCases","InsurancePolicyClaims","InsurancePolicyClaims.ipynb","false");
-        await runDemo(newPage,"UseCases","KneeReplacement","KneeReplacementAttributionAnalysis.ipynb","false");
-        await runDemo(newPage,"UseCases","KneeReplacement","PathAnalysisforTotalKneeReplacement.ipynb","true");
-        await runDemo(newPage,"UseCases","ManufacturingDefects","ManufacturingDefects.ipynb","true");
-        await runDemo(newPage,"UseCases","NYC-taxi-4d","NYC-taxi-geospatial-visual.ipynb","true");
-        await runDemo(newPage,"UseCases","NYC-taxi-4d","NYC-taxi-timeseries.ipynb","false");
-        await runDemo(newPage,"UseCases","Outlier_Analysis_Transformation","Outlier_Analysis_Transformation.ipynb","false");
-        await runDemo(newPage,"UseCases","SalesForecasting","SalesForecastARIMA.ipynb","true");
-        await runDemo(newPage,"UseCases","SurvivalAnalysis","SurvivalAnalysis_Python.ipynb","true");
-        await runDemo(newPage,"UseCases","Telco_4d_Analytics","telco_network_coverage.ipynb","true");
-        await runDemo(newPage,"UseCases","TextProcessing_TF_IDF","TF_IDF_Workflow_Pythonista.ipynb","true");
-        await runDemo(newPage,"UseCases","TextProcessing_TF_IDF","TF_IDF_Workflow_SQL.ipynb","false");
-        await runDemo(newPage,"UseCases","TextProcessing_TF_IDF","TF_IDF_Workflow.ipynb","true");
-        await runDemo(newPage,"UseCases","TimeSeriesAnalysis","TimeSeriesAnalysis.ipynb","false");
-        await runDemo(newPage,"UseCases","VantageAnalyticLibrary","Overview_Vantage_Analytic_Library.ipynb","false");
-        await runDemo(newPage,"UseCases","VantageAnalyticLibrary","VAL-Analytic_Algorithms_and_Scoring.ipynb","false");
-        await runDemo(newPage,"UseCases","VantageAnalyticLibrary","VAL-Descriptive_Statistics_and_Transformation.ipynb","false");
-        await runDemo(newPage,"UseCases","VantageAnalyticLibrary","VAL-Hypothesis_Tests.ipynb","false");
-        await runDemo(newPage,"UseCases","VantageAnalyticLibrary","VAL-teradataml-Demo.ipynb","true");
-        await runDemo(newPage,"UseCases","vertex_pipelines_housing_example-BYOM","vertex_pipelines_housing_example-BYOM.ipynb","true");
+        // Click to Run Demos (Done)
+        const pagePromise = context.waitForEvent('page');
+        await page.getByText('open new tab').click();
+        const newPage = await pagePromise;
+        await newPage.waitForLoadState();
+        /*
+        const [newPage] = await Promise.all([
+            page.waitForEvent(page),
+            // This action triggers the new tab
+            page.locator('button >> text=RUN DEMOS USING JUPYTER').click() 
+        ])
+            // Wait for New Page/Tab to Load, Then jump over to new page (Done)
+            await newPage.waitForLoadState();                
+            // title of new tab page
+            //console.log(await newPage.title());       
+            // title of existing page
+            //console.log(await page.title());
+            //const jupyter_url=await newPage.url(); //get the url of the current page
+            //strip jupyter url 
+            //const jUrl = jupyter_url.split("/lab/")[0]
 
-        return ''
-       
- });
+            // Wait for content to appear before moving on.  Jupyter is slow to load (Done)
+            await newPage.waitForSelector('text=ClearScape Analytics Demonstrations via Jupyter');
+
+            // Run through Demos //
+
+            // GettingStarted
+            await runDemo(newPage,"GettingStarted","GettingStarted","GettingStarted.ipynb","false");
+            await runDemo(newPage,"GettingStarted","GettingStarted_Charting","GettingStarted_Charting.ipynb","false");   // ?              
+            await runDemo(newPage,"GettingStarted","GettingStarted_Jupyter_Pandas","GettingStarted_Jupyter_Pandas.ipynb","true");  // This python script has too many inputs
+            
+            // UseCases
+            await runDemo(newPage,"UseCases","AirPassengersTimeSeriesForecasting","AirPassengersTimeSeriesForecasting.ipynb","false");
+            await runDemo(newPage,"UseCases","AustinBikeShare","AustinBikeShare.ipynb","false");
+            await runDemo(newPage,"UseCases","BI_Tools","Dataiku.ipynb","true");
+            await runDemo(newPage,"UseCases","BrokenDigitalEvents","RetailBrokenDigitalJourneysPython.ipynb","true");
+            await runDemo(newPage,"UseCases","Clustering_KMeans","Clustering_KMeans.ipynb","false");
+            await runDemo(newPage,"UseCases","Consumption_Forecasting_BYOM","Energy_Consumption_Forecasting_BYOM.ipynb","true");
+            await runDemo(newPage,"UseCases","CreditCardFraud","CreditCardFraud.ipynb","false");
+            await runDemo(newPage,"UseCases","CustomerBehaviorAnalysis","BehavioralAnalysis.ipynb","true");
+            //await runDemo(newPage,"UseCases","Data_Dictionary","Data_Dictionary_Legacy.ipynb","true");   // hung up on space allocation
+            await runDemo(newPage,"UseCases","Data_Dictionary","Data_Dictionary.ipynb","true");  
+            await runDemo(newPage,"UseCases","DataPrepAndDiscovery","DataPrepAndDiscovery.ipynb","true"); 
+            await runDemo(newPage,"UseCases","DataScienceFlow","DataScienceFlow.ipynb","true"); 
+            await runDemo(newPage,"UseCases","DeepHistory","DeepHistory.ipynb","false");
+            await runDemo(newPage,"UseCases","ESG","ESG_pop.ipynb","true");
+            await runDemo(newPage,"UseCases","ESG","ESG.ipynb","true");
+            await runDemo(newPage,"UseCases","FourierTransform","FourierTransform.ipynb","false");
+            await runDemo(newPage,"UseCases","FSCustomerJourney","FSCustomerJourney.ipynb","false");
+            await runDemo(newPage,"UseCases","GLM_Fraud_Detection_BYOM","GLM_Fraud_Detection_BYOM.ipynb","true");
+            await runDemo(newPage,"UseCases","GLM_Fraud_Detection_InDB","GLM_Fraud_Detection_InDB.ipynb","true");
+            await runDemo(newPage,"UseCases","IndoorSensor","IndoorSensor.ipynb","true");
+            await runDemo(newPage,"UseCases","InsurancePolicyClaims","InsurancePolicyClaims.ipynb","false");
+            await runDemo(newPage,"UseCases","KneeReplacement","KneeReplacementAttributionAnalysis.ipynb","false");
+            await runDemo(newPage,"UseCases","KneeReplacement","PathAnalysisforTotalKneeReplacement.ipynb","true");
+            await runDemo(newPage,"UseCases","ManufacturingDefects","ManufacturingDefects.ipynb","true");
+            await runDemo(newPage,"UseCases","NYC-taxi-4d","NYC-taxi-geospatial-visual.ipynb","true");
+            await runDemo(newPage,"UseCases","NYC-taxi-4d","NYC-taxi-timeseries.ipynb","false");
+            await runDemo(newPage,"UseCases","Outlier_Analysis_Transformation","Outlier_Analysis_Transformation.ipynb","false");
+            await runDemo(newPage,"UseCases","SalesForecasting","SalesForecastARIMA.ipynb","true");
+            await runDemo(newPage,"UseCases","SurvivalAnalysis","SurvivalAnalysis_Python.ipynb","true");
+            await runDemo(newPage,"UseCases","Telco_4d_Analytics","telco_network_coverage.ipynb","true");
+            await runDemo(newPage,"UseCases","TextProcessing_TF_IDF","TF_IDF_Workflow_Pythonista.ipynb","true");
+            await runDemo(newPage,"UseCases","TextProcessing_TF_IDF","TF_IDF_Workflow_SQL.ipynb","false");
+            await runDemo(newPage,"UseCases","TextProcessing_TF_IDF","TF_IDF_Workflow.ipynb","true");
+            await runDemo(newPage,"UseCases","TimeSeriesAnalysis","TimeSeriesAnalysis.ipynb","false");
+            await runDemo(newPage,"UseCases","VantageAnalyticLibrary","Overview_Vantage_Analytic_Library.ipynb","false");
+            await runDemo(newPage,"UseCases","VantageAnalyticLibrary","VAL-Analytic_Algorithms_and_Scoring.ipynb","false");
+            await runDemo(newPage,"UseCases","VantageAnalyticLibrary","VAL-Descriptive_Statistics_and_Transformation.ipynb","false");
+            await runDemo(newPage,"UseCases","VantageAnalyticLibrary","VAL-Hypothesis_Tests.ipynb","false");
+            await runDemo(newPage,"UseCases","VantageAnalyticLibrary","VAL-teradataml-Demo.ipynb","true");
+            await runDemo(newPage,"UseCases","vertex_pipelines_housing_example-BYOM","vertex_pipelines_housing_example-BYOM.ipynb","true");
+
+            return ''
+        */
+    });
+});
  async function runDemo(page: Page, menu: string, submenu: string, demoFile: string, isPythonKernel: string){
     // Go to Main Folder
     await page.waitForSelector('span[title="~/JupyterLabRoot"]');
