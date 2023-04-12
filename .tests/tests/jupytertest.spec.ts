7d9e0c3fcd9f9c6337febe595b740ba79fbd6555
davidtkunz <117479775+davidtkunz@users.noreply.github.com>
2023-04-12 16:40:36 -0700

    Update jupytertest.spec.ts

--- .tests/tests/jupytertest.spec.ts
+++ .tests/tests/jupytertest.spec.ts
@@ -66,12 +66,12 @@ test('verify multiple tabs', async({page})=>{
         await runDemo(page1,"UseCases","Consumption_Forecasting_BYOM","Energy_Consumption_Forecasting_BYOM.ipynb","true");
         await runDemo(page1,"UseCases","CreditCardFraud","CreditCardFraud.ipynb","false");
         await runDemo(page1,"UseCases","CustomerBehaviorAnalysis","BehavioralAnalysis.ipynb","true");
-        //await runDemo(newPage,"UseCases","Data_Dictionary","Data_Dictionary_Legacy.ipynb","true");   // hung up on space allocation
+ /*        //await runDemo(newPage,"UseCases","Data_Dictionary","Data_Dictionary_Legacy.ipynb","true");   // hung up on space allocation
         await runDemo(page1,"UseCases","Data_Dictionary","Data_Dictionary.ipynb","true");  
         await runDemo(page1,"UseCases","DataPrepAndDiscovery","DataPrepAndDiscovery.ipynb","true"); 
         await runDemo(page1,"UseCases","DataScienceFlow","DataScienceFlow.ipynb","true"); 
         await runDemo(page1,"UseCases","DeepHistory","DeepHistory.ipynb","false");
-   /*
+  
         await runDemo(page1,"UseCases","ESG","ESG_pop.ipynb","true");
         await runDemo(page1,"UseCases","ESG","ESG.ipynb","true");
         await runDemo(page1,"UseCases","FourierTransform","FourierTransform.ipynb","false");
