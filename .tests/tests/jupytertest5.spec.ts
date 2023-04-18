195acf7f39149a9ebb88ce96f83a6eaaad558ed9
davidtkunz <117479775+davidtkunz@users.noreply.github.com>
2023-04-18 15:09:26 -0700

    Update jupytertest5.spec.ts

--- .tests/tests/jupytertest5.spec.ts
+++ .tests/tests/jupytertest5.spec.ts
@@ -94,8 +94,6 @@ test('verify multiple tabs', async({page})=>{
         await runDemo(page1,"UseCases","VantageAnalyticLibrary","VAL-Hypothesis_Tests.ipynb","false");
         await runDemo(page1,"UseCases","VantageAnalyticLibrary","VAL-teradataml-Demo.ipynb","true");
         await runDemo(page1,"UseCases","vertex_pipelines_housing_example-BYOM","vertex_pipelines_housing_example-BYOM.ipynb","true");
-
-        return ''
        
  });
  async function runDemo(page: Page, menu: string, submenu: string, demoFile: string, isPythonKernel: string){
