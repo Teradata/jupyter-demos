b9fef6f499fb1cc54f91c9326377a151fa19088e
davidtkunz <117479775+davidtkunz@users.noreply.github.com>
2023-04-21 17:57:12 -0700

    Update jupytertestPython.spec.ts

--- .tests/tests/jupytertestPython.spec.ts
+++ .tests/tests/jupytertestPython.spec.ts
@@ -54,7 +54,7 @@ test('verify multiple tabs', async({page})=>{
     //await runDemo(page1,"UseCases","Data_Dictionary","Data_Dictionary.ipynb","true");            // dismiss for testing - hung up on space allocation
     await runDemo(page1,"UseCases","DataPrepAndDiscovery","DataPrepAndDiscovery.ipynb","true"); 
     await runDemo(page1,"UseCases","DataScienceFlow","DataScienceFlow.ipynb","true"); 
-    //await runDemo(page1,"UseCases","ESG","ESG_pop.ipynb","true");     // Fails on CI, but works running manually
+    //await runDemo(page1,"UseCases","ESG","ESG_pop.ipynb","true");     // Ignore.  Going away.   Fails on CI, but works running manually
     //await runDemo(page1,"UseCases","ESG","ESG.ipynb","true");         // Fails on CI, but works running manually
     await runDemo(page1,"UseCases","GLM_Fraud_Detection_BYOM","GLM_Fraud_Detection_BYOM.ipynb","true");
     await runDemo(page1,"UseCases","GLM_Fraud_Detection_InDB","GLM_Fraud_Detection_InDB.ipynb","true");
