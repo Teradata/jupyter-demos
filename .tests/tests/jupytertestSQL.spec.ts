6e3c5fbad2d0f5c0b173d8396e9128e9ff82ff0a
davidtkunz <117479775+davidtkunz@users.noreply.github.com>
2023-05-12 10:08:14 -0700

    Update jupytertestSQL.spec.ts

--- .tests/tests/jupytertestSQL.spec.ts
+++ .tests/tests/jupytertestSQL.spec.ts
@@ -71,7 +71,7 @@ test('verify multiple tabs', async({page})=>{
     const date = new Date();    
     const strText = date.toDateString() + ' ' + date.toTimeString() + ' ' + demoFile + '\r\n';
     //fs.writeFileSync('./results.log', strText);
-    fs.appendFileSync('../playwright-report/results.log', strText);
+    fs.appendFileSync('./playwright-report/results.log', strText);
 
     // Go to Main Folder
     await page.waitForSelector('span[title="~/JupyterLabRoot"]');
