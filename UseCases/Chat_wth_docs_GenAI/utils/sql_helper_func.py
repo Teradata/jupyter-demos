a358a8da3384bdaa77eeba0331f4f7c2ac8c4c1d
chetan-hirapara <chetan.hirapara@teradata.com>
2023-12-12 13:35:54 +0000

    fixed code conflicts

--- UseCases/Chat_wth_docs_GenAI/utils/sql_helper_func.py
+++ UseCases/Chat_wth_docs_GenAI/utils/sql_helper_func.py
@@ -1,5 +1,6 @@
 # teradata lib
 from teradataml.dataframe.copy_to import copy_to_sql
+from teradataml import execute_sql
 
 #  SQL code
 # delete all the records from table
@@ -16,9 +17,9 @@ def delete_emb_from_sql(table_name, eng):
 # only copy embeddings, if table exists then append
 def copy_emb_to_sql(table_name, tdf):
     try:
-        copy_to_sql(tdf, table_name=table_name,primary_index='id', if_exists='append')
-    except Exception as e:
-        print(e)
+        copy_to_sql(tdf, table_name=table_name,primary_index='product_id', if_exists='append')
+    except:
+        pass
 
 def delete_and_copy_embeddings(table_name, tdf, eng):
     # only delete records if table exists
@@ -30,6 +31,6 @@ def delete_and_copy_embeddings(table_name, tdf, eng):
 
     # only insert records if table exists
     try:
-        copy_to_sql(tdf, table_name=table_name,primary_index='id', if_exists='replace')
-    except Exception as e:
-        print(e)
+        copy_to_sql(tdf, table_name=table_name,primary_index='product_id', if_exists='append')
+    except:
+        pass
