d316c56893cfe2fef4a129f3d5c5ebf4c42a16ab
chetan-hirapara <chetan.hirapara@teradata.com>
2023-12-14 08:00:29 +0000

    added demo using TDApiClient

---
+++ UseCases/Recommendations_product_search/utils/tdapiclient_helper_func.py
@@ -0,0 +1,15 @@
+from tdapiclient import TDApiClient
+
+def generate_embeddings_tdapiclient(tdf, api_key, text_column):
+    #  create authorization
+    auth_info = f'{{ "key": "{api_key}" }}'
+    num_embeddings = 1536
+
+    return TDApiClient.API_Request(
+        tdf,
+        "open-ai-embedding",
+        authorization=auth_info,
+        num_embeddings=num_embeddings,
+        model_name="text-embedding-ada-002",
+        text_column=text_column,
+    )
