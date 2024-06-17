2ff173477bdab43fcba14a6e67b62d1b0a1194be
Nitin Sreeram <nitin.sreeram@teradata.com>
2024-06-17 17:28:08 +0530

    fix: typo

--- .tests/pre_create_env.ts
+++ .tests/pre_create_env.ts
@@ -5,12 +5,16 @@ const CSAE_WORKERS_COUNT = parseInt(process.env.CSAE_WORKERS_COUNT || '1');
 const CSAE_PARALLEL_TESTS_COUNT = parseInt(process.env.CSAE_PARALLEL_TESTS_COUNT || '1');
 const envs:Promise<any>[] = [];
 
-for (let i = 0; i < 1; i++) {
-    for (let i = 0; i < Math.floor(CSAE_WORKERS_COUNT/CSAE_PARALLEL_TESTS_COUNT); i++) {
-        const env = new Environments(`env${i}`, 'us-central', CSAE_ENV_PASSWORD)
-        envs.push(env.create());
-    }
+console.log('CSAE_WORKERS_COUNT:', CSAE_WORKERS_COUNT);
+console.log('CSAE_PARALLEL_TESTS_COUNT:', CSAE_PARALLEL_TESTS_COUNT);
+
+console.log(`Need ${Math.floor(CSAE_WORKERS_COUNT/CSAE_PARALLEL_TESTS_COUNT)} environments`);
+
+for (let i = 0; i < Math.floor(CSAE_WORKERS_COUNT/CSAE_PARALLEL_TESTS_COUNT); i++) {
+    const env = new Environments(`env${i}`, 'us-central', CSAE_ENV_PASSWORD)
+    envs.push(env.create());
 }
+
 console.log(`Creating ${envs.length} environments`);
 Promise.all(envs).then(() => {
     console.log('Environments created');
